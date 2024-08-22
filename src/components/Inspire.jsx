3d1bb4ea4dedfc8743e46c18653629157187c3d6
Preranabrainvire <preranababar9403@gmail.com>
2024-08-22 18:59:28 +0530

    swiper js added

--- src/components/Inspire.jsx
+++ src/components/Inspire.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import slide1 from "/images/slide1.svg";
 import slide2 from  "/images/slide2.svg";
+// import Swiper JS
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper styles
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Inspire = () => {
   return (
@@ -16,13 +24,28 @@ const Inspire = () => {
         <button className='bg-brown  text-white px-8 uppercase font-bold text-sm py-4'>Explore More</button>
        </div>
 
-       <div className='h-full flex max-md:flex-col w-[60%] max-md:gap-y-4 gap-4 '>
-        <img src={slide1} alt="" className='' />
+       <Swiper
+        spaceBetween={10}
+        slidesPerView={2}
+      
+       
+        pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination]}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+       className='h-full flex max-md:flex-col w-[60%] max-md:gap-y-4 gap-4 '>
 
-        <img src={slide2} alt="" />
 
-        <img src={slide2} alt="" className=''/>
-       </div>
+       <SwiperSlide> <img src={slide1} alt="" className='' /></SwiperSlide>
+
+       <SwiperSlide> <img src={slide2} alt="" /></SwiperSlide> 
+
+       <SwiperSlide>  <img src={slide2} alt="" className=''/></SwiperSlide> 
+
+
+       </Swiper>
          </div>
     </section>
   )
