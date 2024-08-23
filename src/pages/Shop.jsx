243c6d15dbdc027066290b1faf9062209802d0f8
Preranabrainvire <preranababar9403@gmail.com>
2024-08-23 11:16:58 +0530

    trying

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/common/Header'
-import Product from '../components/Home/Product'
+import Product from '../components/home/Product'
 import Footer from '../components/common/Footer'
 
 const Shop = () => {
