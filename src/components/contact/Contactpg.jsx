fc1bd39fa54e4538b3cac235438a8c402f2c9596
Preranabrainvire <preranababar9403@gmail.com>
2024-08-24 18:52:17 +0530

    contact is designed and builded

--- src/components/contact/Contactpg.jsx
+++ src/components/contact/Contactpg.jsx
@@ -1,14 +1,102 @@
-import React from 'react'
+import React from "react";
+import contact1 from "/icons/contact1.svg";
+import contact2 from "/icons/contact2.svg";
+import contact3 from "/icons/contact3.svg";
 
 const Contactpg = () => {
   return (
-        <section className='py-20'>
-
-    <div>
-        Contactpg
+    <section className="py-20 ">
+      <div className="max-width">
+        <div className=" pb-20 flex flex-col  items-center">
+          <h2 className=" font-black font-bold text-3xl ">
+            Get In Touch With Us
+          </h2>
+          <p className="text-offgrey w-1/2 text-center ">
+            For More Information About Our Product & Services. Please Feel Free
+            To Drop Us An Email. Our Staff Always Be There To Help You Out. Do
+            Not Hesitate!
+          </p>
         </div>
-    </section>
-  )
-}
 
-export default Contactpg
+        <div className="flex justify-evenly">
+          <div className="flex flex-col gap-y-10 w-[25%] px-10">
+            <div className="flex gap-6">
+              <img src={contact1} alt="" />
+
+              <div>
+                <h4 className="text-lg font-semibold">Address</h4>
+                <p className="text-sm">
+                  236 5th SE Avenue, New York NY10000, United States
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-6">
+              <img src={contact2} alt="" />
+
+              <div>
+                <h4 className="text-lg font-semibold">Phone</h4>
+                <p className="text-sm">Mobile: +(84) 546-6789</p>
+                <p className="text-sm"> Hotline: +(84) 456-6789</p>
+              </div>
+            </div>
+
+            <div className="flex gap-6">
+              <img src={contact3} alt="" />
+
+              <div>
+                <h4 className="text-lg font-semibold">Working Time</h4>
+                <p className="text-sm">Monday-Friday: 9:00 - 22:00 </p>
+                <p className="text-sm"> Saturday-Sunday: 9:00 - 21:00</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="flex flex-col gap-y-1 pb-5">
+              <label>Your name</label>
+              <input
+                type="text"
+                placeholder="abc"
+                className="border-2 border-solid border-offgrey rounded-lg py-3 pr-40 pl-2"
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2 pb-5">
+              <label>Email Address</label>
+              <input
+                type="email"
+                placeholder="abc@gmail.com"
+                className="border-2 border-solid border-offgrey rounded-lg py-3 pr-40 pl-2"
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2 pb-5">
+              <label>Subject</label>
+              <input
+               rows="4"
+                type="text"
+                placeholder="Optional"
+                className="border-2 border-solid border-offgrey rounded-lg py-3 pl-2"
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2 pb-9">
+              <label>Message</label>
+              <textarea 
+               rows="4" cols="50"
+                type="text"
+                placeholder="Hi! i’d like to ask about"
+                className="border-2 border-solid border-offgrey rounded-lg py-2 text-start pr-40 pl-2"
+              />
+            </div>
+
+            <button className="bg-[#B88E2F] text-white px-16 py-3">Submit</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contactpg;
