52e43f068e622d2c6eb38a7bb97bcb8ce9fd46b0
Preranabrainvire <preranababar9403@gmail.com>
2024-08-28 12:40:26 +0530

    signup page created succesfully

--- src/components/Signuplogin.jsx
+++ src/components/Signuplogin.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import signup from "/images/signup.svg";
+import { Link } from "react-router-dom";
 
 const Signuplogin = () => {
   return (
-    <div>Signuplogin</div>
-  )
-}
+    <section className="pb-20 pt-10">
+      <div className="max-width"> 
+      <div className="flex  items-center">
+        <div className="w-1/2 mr-36"><img src={signup} alt="" /></div>
 
-export default Signuplogin
+        <div className="flex flex-col w-1/3 ">
+                <h4 className="text-3xl font-bold pb-3">Create New Account</h4>
+                <p className="text-md text-offgrey pb-6">Please enter your details</p>
+
+                <label className="font-medium pb-2 text-lg">First Name</label>
+                <input type="text" 
+                placeholder="abc.."
+          
+          className="border-solid border-2 border-black  px-10 py-3 mb-4 rounded-lg  "/>
+
+<label className="font-medium pb-2 text-lg">Last Name</label>
+                <input type="text" 
+                placeholder="abc.."
+          
+          className="border-solid border-2 border-black  px-10  mb-4 py-3 rounded-lg  "/>
+
+
+<label className="font-medium pb-2 text-lg">Email Address</label>
+                <input type="email" 
+                placeholder="abc@gmail.com"
+ 
+ className="border-solid border-2 border-black  px-10  mb-4 py-3 rounded-lg  "/>
+              
+
+                <label className="font-medium pb-2 text-lg">Password</label>
+                <input type="password" 
+                placeholder="............"
+                className="border-solid border-2 border-black  px-10  mb-4 py-3  rounded-lg  "
+                />
+
+
+                <button className="bg-black text-white py-4 my-5 rounded-lg  "><a >Signup</a></button>
+
+                <p className="text-lg text-center">You already have a account? <a className="font-bold underline underline-offset-4">Login here</a></p>
+                  </div>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signuplogin;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Signuplogin from '../components/Signuplogin'
+import Header from '../components/common/Header'
+import Contactpg from '../components/contact/Contactpg'
+import Footer from '../components/common/Footer'
+import Card from '../components/common/Card'
 
 const Signup = () => {
   return (
-    <div><Signuplogin/>
+    <div>
+      <Header/>
+      <Signuplogin/>
+      <Card/>
+      <Contactpg/>
+      <Footer/>
     </div>
   )
 }
