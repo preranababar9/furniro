6f2918c65119e25cc64106ae73dc2d475d77fe33
Preranabrainvire <preranababar9403@gmail.com>
2024-08-28 12:59:57 +0530

    responsive signup

--- src/components/Signuplogin.jsx
+++ src/components/Signuplogin.jsx
@@ -5,46 +5,62 @@ import { Link } from "react-router-dom";
 const Signuplogin = () => {
   return (
     <section className="pb-20 pt-10">
-      <div className="max-width"> 
-      <div className="flex  items-center">
-        <div className="w-1/2 mr-36"><img src={signup} alt="" /></div>
-
-        <div className="flex flex-col w-1/3 ">
-                <h4 className="text-3xl font-black font-bold pb-2">Create New Account</h4>
-                <p className="text-md text-offgrey pb-6">Please enter your details</p>
-
-                <label className="font-medium pb-2 text-lg">First Name</label>
-                <input type="text" 
-                placeholder="abc.."
-          
-          className="border-solid border-2 border-offgrey  px-10 py-3 mb-4 rounded-lg  "/>
-
-<label className="font-medium pb-2 text-lg">Last Name</label>
-                <input type="text" 
-                placeholder="abc.."
-          
-          className="border-solid border-2 border-offgrey  px-10  mb-4 py-3 rounded-lg  "/>
+      <div className="max-width">
+        <div className="flex  items-center">
+          <div className="w-1/2 mr-36 max-md:hidden">
+            <img src={signup} alt="" />
+          </div>
 
 
-<label className="font-medium pb-2 text-lg">Email Address</label>
-                <input type="email" 
-                placeholder="abc@gmail.com"
- 
- className="border-solid border-2 border-offgrey  px-10  mb-4 py-3 rounded-lg  "/>
-              
 
-                <label className="font-medium pb-2 text-lg">Password</label>
-                <input type="password" 
-                placeholder="............"
-                className="border-solid border-2 border-offgrey  px-10  mb-4 py-3  rounded-lg  "
-                />
+          <div className="flex flex-col w-1/3 max-md:w-full">
+            <h4 className="text-3xl font-black font-bold pb-2">
+              Create New Account
+            </h4>
+            <p className="text-md text-offgrey pb-6">
+              Please enter your details
+            </p>
 
+            <label className="font-medium pb-2 text-lg">First Name</label>
+            <input
+              type="text"
+              placeholder="abc.."
+              className="border-solid border-2 border-offgrey  px-10 py-3 mb-4 rounded-lg  "
+            />
 
-                <button className="bg-[#B88E2F] text-white py-4 my-5 rounded-lg  "><a className="text-lg">Signup</a></button>
+            <label className="font-medium pb-2 text-lg">Last Name</label>
+            <input
+              type="text"
+              placeholder="abc.."
+              className="border-solid border-2 border-offgrey  px-10  mb-4 py-3 rounded-lg  "
+            />
 
-                <p className="text-lg text-center">You already have a account? <a className="font-bold underline underline-offset-4">Login here</a></p>
-                  </div>
-      </div>
+            <label className="font-medium pb-2 text-lg">Email Address</label>
+            <input
+              type="email"
+              placeholder="abc@gmail.com"
+              className="border-solid border-2 border-offgrey  px-10  mb-4 py-3 rounded-lg  "
+            />
+
+            <label className="font-medium pb-2 text-lg">Password</label>
+            <input
+              type="password"
+              placeholder="............"
+              className="border-solid border-2 border-offgrey  px-10  mb-4 py-3  rounded-lg  "
+            />
+
+            <button className="bg-[#B88E2F] text-white py-4 my-5 rounded-lg  ">
+              <a className="text-lg">Signup</a>
+            </button>
+
+            <p className="text-lg text-center">
+              You already have a account?{" "}
+              <a className="font-bold underline underline-offset-4">
+                Login here
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
