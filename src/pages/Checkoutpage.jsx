b0feb3c18a7e391a78f9346e2781386070f8cd28
Preranabrainvire <preranababar9403@gmail.com>
2024-09-05 17:12:20 +0530

    checkout page created

---
+++ src/pages/Checkoutpage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/common/Header'
+import Card from '../components/common/Card'
+import Footer from '../components/common/Footer'
+
+const Checkoutpage = () => {
+  return (
+
+
+    <div>
+        <Header/>
+        <Card/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Checkoutpage
