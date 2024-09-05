382754f283c9b6ac8900c53b79f2e1345f647776
Preranabrainvire <preranababar9403@gmail.com>
2024-09-05 20:55:16 +0530

    form

--- src/components/common/Checkout.jsx
+++ src/components/common/Checkout.jsx
@@ -4,14 +4,14 @@ const Checkout = () => {
   return (
     <section className="py-20">
       <div className="max-width">
-        <div className=" flex justify-evenly  ">
-          <div className=" w-1/2 flex flex-col items-center">
+        <div className=" flex max-md:flex-col justify-evenly  ">
+          <div className=" lg:w-1/2 flex flex-col items-center">
             <h3 className="text-center text-3xl font-bold mb-5">
               Billing Details
             </h3>
             <form className="">
-              <div className="flex gap-5">
-                <div class=" py-5 bg-white rounded-lg ">
+              <div className="flex ">
+                <div class=" py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     First Name
                   </label>
@@ -22,9 +22,9 @@ const Checkout = () => {
                   />
                 </div>
 
-                <div class="py-5 bg-white rounded-lg ">
+                <div class="py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
-                    First Name
+                    Last Name
                   </label>
                   <input
                     class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
@@ -35,18 +35,18 @@ const Checkout = () => {
               </div>
 
               <div>
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 px-5 bg-white rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Country/Region
                   </label>
                   <input
-                    class="text-sm custom-input py-4 border  w-full border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input py-4 border  w-full  border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
                     placeholder=""
                     type="text"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Street Address
                   </label>
@@ -57,7 +57,7 @@ const Checkout = () => {
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Town/City
                   </label>
@@ -68,7 +68,7 @@ const Checkout = () => {
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     ZIP Code
                   </label>
@@ -79,7 +79,7 @@ const Checkout = () => {
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Phone No
                   </label>
@@ -90,7 +90,7 @@ const Checkout = () => {
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Email Address
                   </label>
@@ -102,7 +102,7 @@ const Checkout = () => {
                 </div>
 
                 <input
-                  class="text-sm custom-input w-full py-4 mt-8 px-3 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                  class="text-sm custom-input w-full py-4 mt-8 px-3  border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
                   placeholder="Additional Information"
                   type="text"
                 />
