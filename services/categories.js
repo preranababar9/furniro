5dd311a7b5c6223b6667a1f2e3d8c12141a1351d
Preranabrainvire <preranababar9403@gmail.com>
2024-09-08 11:31:38 +0530

    comments added

--- services/categories.js
+++ services/categories.js
@@ -1,5 +1,7 @@
 import { db } from "../config/firebase";
 
+
+// for storing data in db use set(data) with unique doc(data.id)
 export const addCategory = async (data) => {//added into db
   try {
     const categoryOutput = await db.collection("category").doc(data.id).set(data);
@@ -12,7 +14,11 @@ export const addCategory = async (data) => {//added into db
 
 export const getAllCategoryData = async () => { // fetching from db
   try {
+
+        // Fetch all documents from the 'category' collection in the Firestore database
     const response = await db.collection("category").get();
+
+      // Map over the fetched documents and extract the data from each document
     const categoryData = response.docs.map((doc) =>  doc.data());
     return categoryData;
   } catch (error) {
