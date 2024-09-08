3cb9fd4ae6d6884d79ece1f81ff11f349f3f3e3b
Preranabrainvire <preranababar9403@gmail.com>
2024-09-08 11:43:37 +0530

    comments

--- services/products.js
+++ services/products.js
@@ -1,16 +1,6 @@
 import { Firestore, collection } from "firebase/firestore";
 import { db } from "../config/firebase";
 
-export const getAllProducts = async () => {
-  try {
-    const response = await db.collection("products").get(); //
-    const data = response.docs.map((doc) => doc.data());
-    return data;
-  } catch (error) {
-    return error;
-  }
-};
-
 export const AddProduct = async (data) => {
   try {
     const response = await db.collection("products").doc(data.id).set(data);
@@ -21,10 +11,25 @@ export const AddProduct = async (data) => {
   }
 };
 
-export const getProductsById = async (id) => {
+
+
+export const getAllProducts = async () => {
+  try {
+    const response = await db.collection("products").get(); //
+    const data = response.docs.map((doc) => doc.data());
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+
+
+export const getProductsById = async (id) => { // Fetching a specific product by its ID.
   try {
     const response = await db.collection("products").doc(id).get(); //for fetching product by id for product detail page 
-    const data =  response.data();
+    
+    const data =  response.data(); //data() method returns the document's contents in the form of a JavaScript object.
     return data;
   } catch (error) {
     return error;
