90b4c2e5a6ff103d37bf252992c259082920c5e7
Preranabrainvire <preranababar9403@gmail.com>
2024-09-11 10:39:28 +0530

    cart design changed

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -19,16 +19,16 @@ const Cart = () => {
     <section className="py-20">
       <div className="max-width">
       <h1 className="text-4xl font-bold text-center mb-10">Your Shopping Cart</h1>
-        <div className=" flex w-full max-md:flex-col  items-center lg:justify-around  bg-white gap-8  text-black text-sm">
+        <div className=" flex w-full max-md:flex-col  items-start lg:justify-around  bg-white gap-8  text-black text-sm">
           
           <div className=" flex flex-col justify-start gap-5">
             {cartItems.map((item, index) => (
               <div key={index} className="flex max-md:flex-col  justify-between  gap-2 lg:gap-20 items-center">
-                <div className="flex  lg:gap-10  gap-4  items-center">
+                <div className="flex  lg:gap-14  gap-4  items-center">
                   <img
                     src={item.imageUrl}
                     alt={item.title}
-                    className="rounded-md lg:h-[200px] h-[100px] lg:w-full w-3/4"
+                    className="rounded-md lg:h-[180px] h-[100px] lg:w-[300px] w-3/4"
                   />
                   <div className="flex flex-col w-full">
                     <h1 className="lg:text-2xl text-lg font-bold">{item.title}</h1>
@@ -58,7 +58,7 @@ const Cart = () => {
             ))}
           </div>
           {cartItems.length > 0 ? (
-            <div className="flex flex-col  ">
+            <div className="flex flex-col mt-20 ">
               <h1 className="text-2xl font-bold">Total: ${getCartTotal()}</h1>
               <button
                 className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
