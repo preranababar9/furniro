c589a8a963ce238c82f218280099e6cc018e5278
Preranabrainvire <preranababar9403@gmail.com>
2024-09-18 12:30:24 +0530

    billing page designed and responsive

--- src/components/common/Checkout.jsx
+++ src/components/common/Checkout.jsx
@@ -8,30 +8,30 @@ const Checkout = () => {
   return (
     <section className="py-20">
       <div className="max-width">
-        <div className=" flex max-md:flex-col justify-evenly  ">
-          <div className=" lg:w-1/2 flex flex-col items-center">
+        <div className=" flex max-md:flex-col w-full gap-20  max-md:items-center  ">
+          <div className=" flex flex-col w-1/2 items-center">
             <h3 className="text-center text-3xl font-bold mb-5">
               Billing Details
             </h3>
             <form className="">
               <div className="flex ">
-                <div class=" py-5 bg-white px-5 rounded-lg ">
+                <div class=" py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     First Name
                   </label>
                   <input
-                    class="text-sm custom-input py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[132px] w-[182px] h-12 "
                     placeholder=""
                     type="text"
                   />
                 </div>
 
-                <div class="py-5 bg-white px-5 rounded-lg ">
+                <div class="py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Last Name
                   </label>
                   <input
-                    class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[132px] w-[182px] h-12 "
                     placeholder=""
                     type="text"
                   />
@@ -39,115 +39,114 @@ const Checkout = () => {
               </div>
 
               <div>
-                <div class=" w-full py-5 px-5 bg-white rounded-lg ">
+                <div class=" w-full py-5 px-4 bg-white rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Country/Region
                   </label>
                   <input
-                    class="text-sm custom-input py-4 border  w-full  border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input py-4 border  w-[400px]  border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12"
                     placeholder=""
                     type="text"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
+                <div class=" w-full py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Street Address
                   </label>
                   <input
-                    class="text-sm custom-input w-full py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12  w-[400px] "
                     placeholder=""
                     type="text"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
+                <div class=" w-full py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Town/City
                   </label>
                   <input
-                    class="text-sm custom-input w-full py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12  w-[400px] "
                     placeholder=""
                     type="text"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
+                <div class=" w-full py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     ZIP Code
                   </label>
                   <input
-                    class="text-sm custom-input w-full py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12  w-[400px] "
                     placeholder=""
                     type="number"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
+                <div class=" w-full py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Phone No
                   </label>
                   <input
-                    class="text-sm custom-input w-full py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input  py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12  w-[400px] "
                     placeholder=""
                     type="number"
                   />
                 </div>
 
-                <div class=" w-full py-5 bg-white px-5 rounded-lg ">
+                <div class=" w-full py-5 bg-white px-4 rounded-lg ">
                   <label class="block text-gray-700 text-sm font-semibold mb-4">
                     Email Address
                   </label>
                   <input
-                    class="text-sm custom-input w-full py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                    class="text-sm custom-input py-4 border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12  w-[400px] "
                     placeholder=""
                     type="email"
                   />
                 </div>
 
-                <input
-                  class="text-sm custom-input w-full py-4 mt-8 px-3  border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100"
+                {/* <input
+                  class="text-sm custom-input w-full py-4 mt-8 px-3  border border-gray-300 rounded-lg shadow-sm transition duration-300 ease-in-out transform focus:-translate-y-1 focus:outline-blue-300 hover:shadow-lg hover:border-blue-300 bg-gray-100 max-md:w-[300px] h-12"
                   placeholder="Additional Information"
                   type="text"
-                />
+                /> */}
               </div>
             </form>
           </div>
 
-          <div className="mt-10 w-1/2">
-            <div className="flex flex-col gap-y-2">
-              <div className="flex justify-evenly  pb-5">
-                <p className="text-xl font-bold">Product</p>
-                <p className="text-xl font-bold">Subtotal</p>
-              </div>
-              {cartItems.map((item, index) => (
-                <div key={index} className="flex  justify-evenly pb-2">
-                  <p className="text-lg  text-offgrey">
-                    {item.title} X {item.quantity}
-                  </p>
-                  <p className="text-lg  ">
-                    {" "}
-                    Rs {new Intl.NumberFormat().format(item.price)}{" "}
-                  </p>
-                </div>
-              ))}
+          <div className="mt-10  lg:w-1/2  mx-auto overflow-auto ">
+          <table class="table-auto lg:w-3/4  text-left ">
+          <thead>
+          <tr>
+             
+          <th class="px-2  py-3 text-gray-900 lg:text-2xl text-xl   ">Product</th>
+          <th class="px-2   py-3 text-gray-900 lg:text-2xl  text-xl   ">Subtotal</th>
+               
+                </tr>
+                </thead>
 
-              {/* <div className="flex justify-evenly pb-5">
-                             <p className="text-lg ">Subtotal</p>
-                             <p className="text-lg ">price</p>
-                     </div> */}
+                <tbody>
+                {cartItems.map((item, index) => (
+                <tr>
+           
+             
+             <td class="px-4 py-3 lg:text-xl text-md "> {item.title} X {item.quantity}</td>
+             <td class="px-4 py-3 lg:text-xl text-md  ">  Rs {new Intl.NumberFormat().format(item.price)}{" "}</td>
+            
+     </tr> 
+       ))} 
+       <tr>
+           
+       <td class="px-4 lg:text-2xl text-xl font-bold    py-3">Total</td>
+          
+       <td class="px-4 py-3 lg:text-2xl text-lg text-brown font-bold ">Rs {new Intl.NumberFormat().format(getCartTotal())}</td>
+       </tr>
+ </tbody>
 
-              <div className="flex justify-evenly pb-5">
-                <p className="text-lg ">Total</p>
-                <p className="text-2xl text-brown font-bold ">
-                  {" "}
-                  Rs {new Intl.NumberFormat().format(getCartTotal())}
-                </p>
-              </div>
-            </div>
+            </table>
           </div>
 
-          <div></div>
+
         </div>
       </div>
     </section>
